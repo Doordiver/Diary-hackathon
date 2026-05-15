7880d1d6aea8c9db648059f5abf6c9c58c5ba14c
Thomas Barkshire <thomasbarkshire@gmail.com>
2026-05-15 14:49:03 +0100

    Built an html and set the DB to auto input date

--- server/DB/diary.sql
+++ server/DB/diary.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS diary;
 
 CREATE TABLE diary (
     entry_id INT GENERATED ALWAYS AS IDENTITY,
-    entry_date DATE NOT NULL,
+    entry_date DATE DEFAULT GETDATE(),
     entry_body VARCHAR(900) NOT NULL, 
     category VARCHAR(50) NOT NULL,
     PRIMARY KEY (entry_id)
